3702af877ecad7fd36211b9a9ee1eec6cb1ecb37
ThomasChun <tchun2055@gmail.com>
2019-10-02 13:42:05 -0700

    updated postman scripts

--- postman/install.sh
+++ postman/install.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 ### install Postman -- old script
-# echo BEGIN installing Postman
-# echo "wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz"
-# wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
-# echo ""
-# echo "sudo tar -xzf postman.tar.gz -C /opt"
-# sudo tar -xzf postman.tar.gz -C /opt
-# echo ""
-# echo "rm postman.tar.gz"
-# rm postman.tar.gz
-# echo ""
-# echo "sudo ln -s /opt/Postman/Postman /usr/bin/postman"
-# sudo ln -s /opt/Postman/Postman /usr/bin/postman
-# echo ""
-# echo "apt-get install libgconf-2-4"
-# sudo apt-get install libgconf-2-4
-# echo ""
-# echo "sudo apt --fix-broken install"
-# sudo apt --fix-broken install -y
-# echo ""
-# echo "wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.png"
-# wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.png
-# echo ""
-# echo "sudo mv postman.png /usr/share/pixmaps/"
-# sudo mv postman.png /usr/share/pixmaps/
-# echo ""
-# echo "wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.desktop"
-# wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.desktop
-# echo ""
-# echo "sudo mv postman.desktop /usr/share/applications/"
-# sudo mv postman.desktop /usr/share/applications/postman.desktop
-# echo ""
-# echo END installing Postman
-# echo ""
-# echo ""
-# echo ""
-# echo ""
+echo BEGIN installing Postman
+echo "wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz"
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+echo ""
+echo "sudo tar -xzf postman.tar.gz -C /opt"
+sudo tar -xzf postman.tar.gz -C /opt
+echo ""
+echo "rm postman.tar.gz"
+rm postman.tar.gz
+echo ""
+echo "sudo ln -s /opt/Postman/Postman /usr/bin/postman"
+sudo ln -s /opt/Postman/Postman /usr/bin/postman
+echo ""
+echo "apt-get install libgconf-2-4"
+sudo apt-get install libgconf-2-4
+echo ""
+echo "sudo apt --fix-broken install"
+sudo apt --fix-broken install -y
+echo ""
+echo "wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.png"
+wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.png
+echo ""
+echo "sudo mv postman.png /usr/share/pixmaps/"
+sudo mv postman.png /usr/share/pixmaps/
+echo ""
+echo "wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.desktop"
+wget https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/postman/postman.desktop
+echo ""
+echo "sudo mv postman.desktop /usr/share/applications/"
+sudo mv postman.desktop /usr/share/applications/postman.desktop
+echo ""
+echo END installing Postman
+echo ""
+echo ""
+echo ""
+echo ""
 
 
 
