f5ddb2ec3e517c15aa2764d0f065947cdf54ea00
ThomasChun <tchun2055@gmail.com>
2019-10-02 14:13:32 -0700

    commented out libgconf-2-4 from postman script

--- postman/install.sh
+++ postman/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-### install Postman -- old script
+### install Postman
 echo BEGIN installing Postman
 echo "wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz"
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
@@ -13,6 +13,7 @@ echo ""
 echo "sudo ln -s /opt/Postman/Postman /usr/bin/postman"
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
 echo ""
+# installing libgconf-2-4 was throwing asking Y/n and then aborting disrupting install flow;
 # echo "apt-get install libgconf-2-4"
 # sudo apt-get install libgconf-2-4
 echo ""
@@ -36,38 +37,3 @@ echo ""
 echo ""
 echo ""
 echo ""
-
-
-
-#!/bin/bash
-### install Postman
-
-#Download file
-echo BEGIN installing Postman
-echo "wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz"
-wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
-echo ""
-
-#Unzip file
-gunzip postman.tar.gz
-
-#Go to directory
-#cd ~/.local/share/applications
-
-#Create Postman.desktop file with text
-sudo echo "
-[Desktop Entry]
-Encoding=UTF-8
-Name=Postman
-Exec=~/usr/bin/postman
-Icon=~/usr/share/pixmaps/postman.png
-Terminal=false
-Type=Application
-Categories=Development;
-" > ~/.local/share/applications/Postman.desktop
-echo ""
-
-sudo mv ~/.local/share/applications/Postman.desktop /usr/share/applications/Postman.desktop
-echo END Installing Postman
-
-/usr/share/applications/Postman.desktop
