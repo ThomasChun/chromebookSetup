a3d2df3880cb7dd5ae9c130efe06a8e037106183
ThomasChun <tchun2055@gmail.com>
2019-10-02 16:48:06 -0700

    updated

--- setupChromebook.sh
+++ setupChromebook.sh
@@ -14,8 +14,8 @@ echo ""
 echo ""
 
 ### install nvm
-# echo "sudo curl -o- https://raw.githubusercontent.com/ThomasChun/chromebookSetup/master/nvm/install.sh | bash"
-# sudo curl -o- https://raw.githubusercontent.com/ThomasChun/chromebookSetup/master/nvm/install.sh | bash
+echo "sudo curl -o- https://raw.githubusercontent.com/ThomasChun/chromebookSetup/master/nvm/install.sh | bash"
+sudo curl -o- https://raw.githubusercontent.com/ThomasChun/chromebookSetup/master/nvm/install.sh | bash
 
 ### run nvm commands to prevent having to close terminal to refresh
 # echo "sudo curl -o- https://raw.githubusercontent.com/ThomasChun/chromebookSetup/master/nvmcmd/install.sh | bash"
