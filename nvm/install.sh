c5eeef0687d84c5f678bb4d750a434015b45fd87
ThomasChun <tchun2055@gmail.com>
2019-10-02 15:45:24 -0700

    updated setupChromebook.sh file

--- nvm/install.sh
+++ nvm/install.sh
@@ -5,10 +5,10 @@ echo ""
 echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash"
 sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash
 echo ""
-echo ""
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# echo ""
+# export NVM_DIR="$HOME/.config/nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 echo ""
 echo ""
 echo END installing nvm
