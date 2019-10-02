17957837f57ec8f4b06bc61e42357f6d14db6153
ThomasChun <tchun2055@gmail.com>
2019-10-02 11:08:55 -0700

    updated nvm script

--- nvm/install.sh
+++ nvm/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 ### install nvm
 echo BEGIN installing nvm
+# echo ""
+# echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
+# sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+# echo ""
 echo ""
-echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
-sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash"
+sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.35.0/install.sh | bash
 echo ""
 echo END installing nvm
 echo ""
