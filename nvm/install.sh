a278926b979d70073baa58c85a32b9f035dcc92f
ThomasChun <tchun2055@gmail.com>
2019-10-02 16:09:10 -0700

    added nvmcmd to run commands

--- nvm/install.sh
+++ nvm/install.sh
@@ -14,9 +14,9 @@ echo ""
 echo END installing nvm
 echo ""
 echo ""
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+sudo export NVM_DIR="$HOME/.config/nvm"
+sudo [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+sudo [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 echo ""
 echo ""
 echo ""
