e71f0f79c581548ab47a4975332b5cb7748cc8ec
ThomasChun <tchun2055@gmail.com>
2019-10-03 08:59:11 -0700

    updates

--- nvmcmd/install.sh
+++ nvmcmd/install.sh
@@ -1,14 +1,34 @@
-#!/bin/bash
-### execute nvm command to continue without having to close and reopen terminal
-echo BEGIN nvm commands
+# #!/bin/bash
+# ### execute nvm command to continue without having to close and reopen terminal
+# echo BEGIN nvm commands
+# echo ""
+# echo ""
+# echo ""
+# export NVM_DIR="$HOME/.config/nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# echo ""
+# echo ""
+# echo END nvm commands
+# echo ""
+# echo ""
+
+
+### install git wget lib32stdc++t libxss1
+echo BEGIN installing git wget lib32stdc++6 libxss1
+sudo apt install -y git wget lib32stdc++6 libxss1
+sudo apt-get -f -y install
+echo "END installing git wget lib32stdc++6 libxssl"
 echo ""
 echo ""
 echo ""
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+echo ""
+
+### install nvm
+echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
+sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 echo ""
 echo ""
-echo END nvm commands
+echo END installing nvm
 echo ""
 echo ""
--- setupChromebook.sh
+++ setupChromebook.sh
@@ -3,24 +3,24 @@
 # sudo curl -o- https://raw.githubusercontent.com/mkpartners/chromebookSetup/master/install.sh | bash
 ###
 
-### install git wget lib32stdc++t libxss1
-echo BEGIN installing git wget lib32stdc++6 libxss1
-sudo apt install -y git wget lib32stdc++6 libxss1
-sudo apt-get -f -y install
-echo "END installing git wget lib32stdc++6 libxssl"
-echo ""
-echo ""
-echo ""
-echo ""
+# ### install git wget lib32stdc++t libxss1
+# echo BEGIN installing git wget lib32stdc++6 libxss1
+# sudo apt install -y git wget lib32stdc++6 libxss1
+# sudo apt-get -f -y install
+# echo "END installing git wget lib32stdc++6 libxssl"
+# echo ""
+# echo ""
+# echo ""
+# echo ""
 
-### install nvm
-echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
-sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
-echo ""
-echo ""
-echo END installing nvm
-echo ""
-echo ""
+# ### install nvm
+# echo "sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash"
+# sudo wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+# echo ""
+# echo ""
+# echo END installing nvm
+# echo ""
+# echo ""
 
 ### install node
 . ~/.nvm/nvm.sh
